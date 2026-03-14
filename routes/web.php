1fed2385df68c7d01e2a1f08ba36da239e4e594e
Arnol <andikamaster13@gmail.com>
2026-03-15 00:11:46 +0700

    LoginController,dan  View

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,12 @@
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ListItemController;
+use App\Http\controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/login', [LoginController::class, 'index']);
 Route::get('/dashboard', [DashboardController::class, 'index']);
 Route::get('/listitem/{id}/{nama}', [ListItemController::class, 'tampilkan']);
