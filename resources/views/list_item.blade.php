3e7bc5bb3691cb3a9f3b9c3db220fc51354b0ff1
nailatzn <tenrinarang@gmail.com>
2026-03-14 21:56:09 +0700

    Add ListItemController, route, and view

---
+++ resources/views/list_item.blade.php
@@ -0,0 +1,8 @@
+<html>
+  <div>
+    <h1>List Item</h1>
+    <p>Kode Item: {{$id}}</p>
+    <p>Nama Item: {{$nama}}</p>
+  </div>
+</html>
+
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ListItemController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/listitem/{id}/{nama}', [ListItemController::class, 'tampilkan']);
